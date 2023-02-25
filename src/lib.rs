402249ee29446e096d442b78d3cbb18a7940714b
Dave Fol <dof5@cornell.edu>
2023-02-24 22:30:29 -0500

    add start bit

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,22 @@ use embedded_hal::digital::v2::OutputPin;
 #[repr(u8)]
 #[derive(Clone, Copy, Debug)]
 pub enum Channel {
-    SGL0 = 0b10000000,
-    SGL1 = 0b11000000,
-    SGL2 = 0b11001000,
-    SGL3 = 0b11010000,
-    SGL4 = 0b10100000,
-    SGL5 = 0b11100000,
-    SGL6 = 0b10110000,
-    SGL7 = 0b11110000,
-    DIF01 = 0b00000000,
-    DIF10 = 0b01000000,
-    DIF23 = 0b00010000,
-    DIF32 = 0b01010000,
-    DIF45 = 0b00100000,
-    DIF54 = 0b01100000,
-    DIF67 = 0b00110000,
-    DIF76 = 0b01110000,
+    SGL0 = 0b11000000,
+    SGL1 = 0b11100000,
+    SGL2 = 0b11100100,
+    SGL3 = 0b11101000,
+    SGL4 = 0b11010000,
+    SGL5 = 0b11110000,
+    SGL6 = 0b11011000,
+    SGL7 = 0b11111000,
+    DIF01 = 0b10000000,
+    DIF10 = 0b10100000,
+    DIF23 = 0b10001000,
+    DIF32 = 0b10101000,
+    DIF45 = 0b10010000,
+    DIF54 = 0b10110000,
+    DIF67 = 0b10011000,
+    DIF76 = 0b10111000,
 }
 
 pub struct TLV0838<SPI, CS>
